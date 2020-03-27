97d5b62ad5c08730deb524240eedc21d77f008ad
Aaron J Mitchell <amitchell1992@knights.ucf.edu>
2020-03-26 21:46:14 -0400

    more changes

---
+++ client/build/precache-manifest.dfa334a648fbcadf51873f0262ac07bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ea005d483a24aa7a5d1b430978b8a32",
+    "url": "/static/media/logo.0ea005d4.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75207b8d1b12400c861a",
+    "url": "/static/js/main.7876cfa6.chunk.js"
+  },
+  {
+    "revision": "811a35e691fa66a3497c",
+    "url": "/static/js/2.61e5ef6e.chunk.js"
+  },
+  {
+    "revision": "75207b8d1b12400c861a",
+    "url": "/static/css/main.5ad54abe.chunk.css"
+  },
+  {
+    "revision": "f8bb1bace6314ce73237c26dc9bcedeb",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/saiyanchu", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds037252.mlab.com:37252/heroku_mmv5f3h1", { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Start the API server
 app.listen(PORT, function() {
